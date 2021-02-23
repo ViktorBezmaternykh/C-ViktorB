ea5a8713486e9b95ed5aca29c958cb6b3a3f1b06
Viktor <victorbezmaternyh@gmail.com>
2021-02-23 11:14:50 +0500

    This is my first lesson's Home task at GB C# cource and

---
+++ Lesson 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine(); // ввод имени лучше смотрится на новой строке
+            Console.Clear(); // Очистим экран для удобства
+            string nowDate = DateTime.Now.ToString("dd MMMM yyyy"); // Нам нужна только дата
+            Console.WriteLine($"Привет, {name}, сегодня {nowDate}.");
+        }
+    }
+}
+ 
